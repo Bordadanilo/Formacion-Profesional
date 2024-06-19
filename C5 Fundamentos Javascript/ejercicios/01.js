e59d0f8811184b6fcd7f24a55427bd629acaff94
bordadanilo <danilborda34@gmail.com>
2024-06-19 19:50:48 -0300

    Actualizado

--- C5 Fundamentos Javascript/ejercicios/01.js
+++ C5 Fundamentos Javascript/ejercicios/01.js
@@ -1,7 +1,7 @@
 // Crea una variable de tipo string.
 // Reemplaza el valor de null por el correspondiente.
 
-const nuevoString = "Holaa";
+const nuevoString = "Hola";
 
 
 module.exports = nuevoString;
